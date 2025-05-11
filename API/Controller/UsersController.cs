53c05563de90f6f54460adf87faf01231913753e
LuckyPunch11 <krokhmaldavid@gmail.com>
2025-05-11 19:29:55 +0300

    03 Authentication

--- API/Controller/UsersController.cs
+++ API/Controller/UsersController.cs
@@ -1,17 +1,18 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controller
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class UsersController : ControllerBase
+    public class UsersController : BaseApiController
     {
         private readonly DataContext _context;
 
